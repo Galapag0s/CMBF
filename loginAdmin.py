754c086ae0253bbfe6de33181689168de9d0b2d4
at <at@localhost.localdomain>
2019-09-18 20:41:35 -0400

    Added Code to Reboot System

--- loginAdmin.py
+++ loginAdmin.py
@@ -45,7 +45,10 @@ print(cookie)
 
 #Turn Remove View Off
 url = "https://"+ host + "/cgi-bin/return.cgi"
+#data = {
+#    "command":"<Send><seid>"+ cookie +"</seid><name>SLIDES_ALWAYS_SEND</name><value>1</value><name>SLIDES_PASSWORD</name><value>0</value><name>SLIDES_SECOND</name><value>0</value></Send>"
+#}
 data = {
-    "command":"<Send><seid>"+ cookie +"</seid><name>SLIDES_ALWAYS_SEND</name><value>1</value><name>SLIDES_PASSWORD</name><value>0</value><name>SLIDES_SECOND</name><value>0</value></Send>"
+    "command" :	"<Send><seid>" + cookie + "</seid><Factory>reboot</Factory></Send>"
 }
 remoteViewOn = requests.post(url=url,data=data, verify=False)
