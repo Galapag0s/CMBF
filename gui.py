c2fcfd31554c0b3ae2f95cf4802aee5a8b8b8c9c
Galapag0s <47836093+Galapag0s@users.noreply.github.com>
2019-12-03 18:28:47 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -22,22 +22,28 @@ import creston
 
 urllib3.disable_warnings()
 
+#Command to print ascii art cause all cool hacker tools have ascii art!
 def printAsciiTitle():
         asciiArt = text2art("CMBF",font="graffiti")
         print(asciiArt)
 
 printAsciiTitle()
 
+#Print the welcome message cause we are cordial and polite
 print(" Welcome to the Creston Projector Attack Framework")
 print(" To start type a command, or type \'help\' to see available options")
 print(" To learn more about a command type \'help [command]\'")
 
+#Array to Hold Hosts
 live_hosts = []
+#Array To Hold Models
 model_dict = []
 
 while True:
+	#Get User Input
 	command_input = input(" > ")
-
+	
+	#Print out the basic help menu
 	if command_input == 'help' and len(command_input) == 4:
 		print(" Commands")
 		print(" network_scan.... This will scan the network for projectors")
@@ -50,7 +56,8 @@ while True:
 		print(" restore......... This will restore the device to factory settings")
 		print(" web_dos......... This will begin sending web requests to the target device")
 		print(" code_cycle...... This will being causing the connect code to change\n")
-
+	
+	#Printn out help messages for each individual fucntion
 	if command_input[:4] == 'help' and len(command_input) > 4:
 		if 'network_scan' in command_input:
 			print(" This funciton will scan the network for devices with http open")
@@ -92,58 +99,84 @@ while True:
 			print(" This function will begin changing the connect code as quickly as the system will allow")
 			print(" To run, type \'code_cycle\' followed by an IP, the username, and the password")
 			print(" Example: code_cycle 10.10.10.10 username password\n")
-
+	
+	#Clear the Screen if its feeling too cluttered
 	elif command_input == 'clear':
 		print('\n' * 40)
 
+	#Get the user input for the network scan
 	elif command_input[:12] == 'network_scan':
+		#Grab the ip range for the scan
 		command_input = command_input.split(" ")
 		ip_range = command_input[1]
+		#Scan network and return the live hosts
 		live_hosts = creston.network_scan(ip_range)
-
+	
+	#Get the user input to manually add a host
 	elif command_input[:8] == 'add_host':
+		#Grab the host IP to add
 		command_input = command_input.split(" ")
 		live = command_input[1]
+		#Add the specified IP
 		live_hosts.append(live)
-
+		
+	#Print out all live hosts
 	elif command_input[:11] == 'print_hosts':
-		print(live_hosts)
-
+		print("Live Hosts")
+		#Loop through all live hosts
+		for host in live_hosts:
+			print(host)
+	
+	#Run the model scan on all live hosts
 	elif command_input[:10] == 'model_scan':
 		model_dict = creston.model_type(live_hosts)
 
+	#Print the models
 	elif command_input[:11] == 'print_model':
-		print(model_dict)
+		print("Models")
+		for host in model_dict:
+			print(host + " : " + model_dict[host])
 
+	#Grab the user input for a reboot
 	elif command_input[:6] == 'reboot':
+		#Grab username and password for input
 		command_input = command_input.split(" ")
 		username = command_input[1]
 		password = command_input[2]
+		#loop through hosts and start rebooting
 		for hosts in model_dict:
 			creston.reboot(hosts,model_dict[hosts],username,password)
-
+	
+	#Grab the user input for a pass change
 	elif command_input[:11] == 'pass_change':
+		#Grab username, current password, and new password
 		comand_input = command_input.split(" ")
 		username = command_input[1]
 		password = command_input[2]
 		newpass = command_input[3]
+		#Run through change passwordds based on the model
 		for hosts in model_dict:
 			if model_dict[hosts] == 'OG':
 				creston.change_pass(hosts,model_dict[hosts],username,password,newpass)
 			else:
 				pass
-
+	#Grab the input and perform a factory restore
 	elif command_input[:7] == 'restore':
+		#Loop through the host and perform a factory restore
 		for hosts in model_dict:
 			creston.restore(hosts,model_dict[hosts])
-
+			
+	#Begin sending web requests form the projects
+	#This is a proof of concept and does not contianully send requets
 	elif command_input[:7] == 'web_dos':
+		#Grab host, username, password, and webserver
 		command_input == command_input.split(" ")
 		host = command_input[1]
 		username = command_input[2]
 		password = command_input[3]
 		webserver = command_input[4]
 		creston.web_dos(host,username,password,webserver)
-
+	
+	#Quit the program
 	elif command_input == 'exit':
 		exit()
